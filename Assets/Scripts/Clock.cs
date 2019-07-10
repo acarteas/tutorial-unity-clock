21ea98fbaf69056967359fb93c3d10d86a75d613
Adam Carter <mail@adam-carter.com>
2019-07-10 08:50:34 -0700

    added finished tutorial along with Windows x64 binary.

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+   public Transform hoursTransform;
+   public Transform minutesTransform;
+   public Transform secondsTransform;
+   public bool continuous;
+
+   private const float DEGREES_PER_HOUR = 30f;
+   private const float DEGREES_PER_MINUTE = 6f;
+   private const float DEGREES_PER_SECOND = 6f;
+
+   void Awake()
+   {
+      //Shows a message in the bottom Unity toolbar
+      //Debug.Log("Hello, World!");
+   }
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   void Update()
+   {
+      if(continuous == true)
+      {
+         UpdateContinuous();
+      }
+      else
+      {
+         UpdateDiscrete();
+      }
+   }
+
+   void UpdateContinuous()
+   {
+      //set clock rotations
+      TimeSpan time = DateTime.Now.TimeOfDay;
+      hoursTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalHours * DEGREES_PER_HOUR, 0f);
+      minutesTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalMinutes * DEGREES_PER_MINUTE, 0f);
+      secondsTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalSeconds * DEGREES_PER_SECOND, 0f);
+   }
+
+   void UpdateDiscrete()
+   {
+      //set clock rotations
+      DateTime now = DateTime.Now;
+      hoursTransform.localRotation = Quaternion.Euler(0f, now.Hour * DEGREES_PER_HOUR, 0f);
+      minutesTransform.localRotation = Quaternion.Euler(0f, now.Minute * DEGREES_PER_MINUTE, 0f);
+      secondsTransform.localRotation = Quaternion.Euler(0f, now.Second * DEGREES_PER_SECOND, 0f);
+   }
+}
